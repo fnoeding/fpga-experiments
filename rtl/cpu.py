f56cc94d591571e03ff49ba73218377491fae5f1
Florian Noeding <florian@noeding.com>
2022-12-30 15:45:19 -0800

    basics of decoder

--- rtl/cpu.py
+++ rtl/cpu.py
@@ -6,6 +6,18 @@ from amaranth.cli import main
 from amaranth.sim import Simulator, Settle, Tick
 
 
+OPCODE_OP_IMM = 0b0010011
+OPCODE_OP = 0b0110011
+OPCODE_LOAD = 0b0000011
+OPCODE_STORE = 0b0100011
+OPCODE_BRANCH = 0b1100011
+OPCODE_CUSTOM0 = 0b0001011
+OPCODE_LUI = 0b0110111
+OPCODE_AUIPC = 0b0010111
+OPCODE_JALR = 0b1100111
+OPCODE_JAL = 0b1101111
+
+
 FUNCT3_OP_ADD = 0b000
 FUNCT3_OP_SLL = 0b001
 FUNCT3_OP_SLT = 0b010
@@ -99,6 +111,61 @@ class RegisterFile(Elaboratable):
         return m
 
 
+class InstructionDecoder(Elaboratable):
+    def __init__(self):
+        self.i_instruction = Signal(32)
+
+        self.o_rs1 = Signal(5)
+        self.o_rs2 = Signal(5)
+        self.o_rd = Signal(5)
+        self.o_rd_we = Signal(1)
+        self.o_funct3 = Signal(3)
+        self.o_funct7 = Signal(7)
+        self.o_invalid = Signal(1)
+        self.o_imm = SignedSignal(32)
+        self.o_has_imm = Signal(1)
+
+        self.ports = []
+        for x in dir(self):
+            if x.startswith("o_") or x.startswith("i_"):
+                self.ports.append(getattr(self, x))
+
+    def elaborate(self, _: Platform) -> Module:
+        m = Module()
+
+        m.d.comb += self.o_invalid.eq(0)
+
+        opcode = self.i_instruction[0:7]
+
+        with m.Switch(opcode):
+            with m.Case(OPCODE_OP_IMM):
+                m.d.comb += [
+                    self.o_rd.eq(self.i_instruction[7:11]),
+                    self.o_rd_we.eq(1),
+                    self.o_funct3.eq(self.i_instruction[12:15]),
+                    self.o_rs1.eq(self.i_instruction[15:20]),
+                    self.o_rs2.eq(0),
+                    self.o_imm.eq(self.i_instruction[20:32]),
+                    self.o_has_imm.eq(1),
+                    self.o_funct7.eq(0),
+                ]
+            with m.Case(OPCODE_OP):
+                m.d.comb += [
+                    self.o_rd.eq(self.i_instruction[7:11]),
+                    self.o_rd_we.eq(1),
+                    self.o_funct3.eq(self.i_instruction[12:15]),
+                    self.o_rs1.eq(self.i_instruction[15:20]),
+                    self.o_rs2.eq(self.i_instruction[20:25]),
+                    self.o_imm.eq(0),
+                    self.o_has_imm.eq(0),
+                    self.o_funct7.eq(self.i_instruction[25:32]),
+                ]
+            with m.Default():
+                m.d.comb += self.o_invalid.eq(1)
+
+        return m
+
+
 def test_alu():
     dut = ALU()
     sim = Simulator(dut)
@@ -158,16 +225,6 @@ def test_register_file():
     sim = Simulator(dut)
     sim.add_clock(1e-6)
 
-    # def check(a, b, expected, signed=True):
-    #    yield dut.i_data1.eq(a)
-    #    yield dut.i_data2.eq(b)
-    #    yield Settle()
-    #    if signed:
-    #        result = yield dut.o_result
-    #    else:
-    #        result = yield dut.o_result.as_unsigned()
-    #    assert result == expected
-
     def bench():
         yield dut.i_select_rs1.eq(0)
         yield dut.i_select_rs2.eq(1)
@@ -177,26 +234,71 @@ def test_register_file():
         yield Settle()
 
         # all registers are reset to zero
-        rs1 = yield dut.o_rs1_value
-        rs2 = yield dut.o_rs2_value
-        assert rs1 == 0
-        assert rs2 == 0
+        assert (yield dut.o_rs1_value) == 0
+        assert (yield dut.o_rs2_value) == 0
 
         # write to register 1
         yield dut.i_we.eq(1)
-        rs2 = yield dut.o_rs2_value
-        assert rs2 == 0, "register is only written on pos edge"
+        assert (yield dut.o_rs2_value) == 0, "register is only written on pos edge"
         yield Tick()
         yield Settle()
-        rs2 = yield dut.o_rs2_value
-        assert rs2 == 42
+        assert (yield dut.o_rs2_value) == 42
 
         # write to register 0 - which is hard coded to zero
         yield dut.i_select_rd.eq(0)
         yield Tick()
         yield Settle()
-        rs1 = yield dut.o_rs1_value
-        assert rs1 == 0, "register zero cannot be written"
+        assert (yield dut.o_rs1_value) == 0, "register zero cannot be written"
+
+        # data is only written on Tick
+        yield dut.i_select_rd.eq(1)
+        yield dut.i_data.eq(21)
+        yield dut.i_we.eq(0)
+        yield Tick()
+        yield Settle()
+        assert (yield dut.o_rs2_value) == 42, "data is only written when we is on"
+
+    sim.add_process(bench)
+    sim.run()
+
+
+def test_instruction_decoder():
+    dut = InstructionDecoder()
+    sim = Simulator(dut)
+
+    def bench():
+        # invalid
+        yield dut.i_instruction.eq(0)
+        yield Settle()
+        assert (yield dut.o_invalid) == 1
+
+        # addi x1, x0, 42
+        yield dut.i_instruction.eq(0x02A00093)
+        yield Settle()
+
+        assert (yield dut.o_rs1) == 0
+        assert (yield dut.o_rs2) == 0
+        assert (yield dut.o_rd) == 1
+        assert (yield dut.o_rd_we) == 1
+        assert (yield dut.o_funct3) == FUNCT3_OP_ADD
+        assert (yield dut.o_funct7) == 0
+        assert (yield dut.o_invalid) == 0
+        assert (yield dut.o_imm) == 42
+        assert (yield dut.o_has_imm) == 1
+
+        # add x2, x0, x1
+        yield dut.i_instruction.eq(0x00100133)
+        yield Settle()
+
+        assert (yield dut.o_rs1) == 0
+        assert (yield dut.o_rs2) == 1
+        assert (yield dut.o_rd) == 2
+        assert (yield dut.o_rd_we) == 1
+        assert (yield dut.o_funct3) == FUNCT3_OP_ADD
+        assert (yield dut.o_funct7) == 0
+        assert (yield dut.o_invalid) == 0
+        assert (yield dut.o_imm) == 0
+        assert (yield dut.o_has_imm) == 0
 
     sim.add_process(bench)
     sim.run()
